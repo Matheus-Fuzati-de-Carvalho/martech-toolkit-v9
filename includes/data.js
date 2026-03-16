a320963bdf8413ea3460a1272e5dea70bfc968e3
Matheus Fuzati <fuzatimatheus@gmail.com>
2026-03-16 20:52:47 -0300

    -   -

--- includes/data.js
+++ includes/data.js
@@ -1,13 +1,14 @@
-const project_id = dataform.projectConfig.defaultDatabase;
+// Captura as variáveis do workflow_settings.yaml ou usa o padrão Magalu
+const raw_ga4_dataset = dataform.projectConfig.vars.raw_ga4_dataset || "raw_ga4";
+const raw_ads_dataset = dataform.projectConfig.vars.raw_ads_dataset || "raw_ads";
+const raw_ads_table = dataform.projectConfig.vars.raw_ads_table || "ad_CampaignBasicStats_987654321";
 const flavor = dataform.projectConfig.vars.flavor || "full";
 
 module.exports = {
-  project_id,
-  raw_ga4: dataform.projectConfig.vars.raw_ga4_dataset,
-  raw_ads: dataform.projectConfig.vars.raw_ads_dataset,
-  raw_ads_table: dataform.projectConfig.vars.raw_ads_table,
-
-  // Ativação baseada no Flavor
+  project_id: dataform.projectConfig.defaultDatabase,
+  raw_ga4: raw_ga4_dataset,
+  raw_ads: raw_ads_dataset,
+  raw_ads_table: raw_ads_table,
   enable_marketing: ["marketing", "full"].includes(flavor),
   enable_retail_media: ["retail_media", "full"].includes(flavor)
 };
