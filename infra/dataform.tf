a7a3843c79bf63c850cae9a850945d8fa42b4280
Matheus Fuzati <fuzatimatheus@gmail.com>
2026-03-17 17:36:27 -0300

    ajuste goole provider beta

--- infra/dataform.tf
+++ infra/dataform.tf
@@ -1,11 +1,12 @@
 resource "google_dataform_repository" "repo" {
+  provider = google-beta
   name     = "toolkit-martech-v8"
   region   = var.region
 
   git_remote_settings {
-    url                                 = var.github_repo_url
-    default_branch                      = "main"
-    authentication_token_secret_version  = "projects/${var.project_id}/secrets/dataform-github-token/versions/latest"
+    url                = var.github_repo_url
+    default_branch     = "main"
+    authentication_token_secret_version = "projects/${var.project_id}/secrets/dataform-github-token/versions/latest"
   }
 
   workspace_compilation_overrides {
@@ -14,5 +15,5 @@ resource "google_dataform_repository" "repo" {
     table_prefix     = ""
   }
 
-  depends_on = [time_sleep.wait_for_iam]
+  depends_on = [time_sleep.wait_for_iam] 
 }
